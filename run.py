d54bf5113f250dfdbead5ff47bc5f8ca0f58d3e2
Neillcllghn <neillcllghn@outlook.com>
2022-12-11 11:28:08 +0000

    Providing a function to allow player to enter a username.

--- run.py
+++ run.py
@@ -3,3 +3,19 @@ from random import randint
 HIDDEN_BOARD = [[' ']*7 for x in range(6)]
 GUESS_PATTERN = [[' ']*7 for x in range(6)]
 let_to_nums={'A': 0,'B': 1, 'C': 2,'D': 3,'E': 4,'F': 5,'G': 6}
+
+
+def input_details():
+    """
+    This function allows the user/player to enter a username.
+    Inspired by Love Sandwiches CI project.
+    """
+    print('Please enter your username.')
+    print('username must not contain numbers or/and special characters')
+    print('Must be no longer than 6 characters')
+    print("Example: Neocal etc.....\n")
+
+    input("Enter your username here:\n")
+
+
+input_details()
