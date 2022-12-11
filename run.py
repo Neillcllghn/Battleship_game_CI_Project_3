d2439dc91d2390e00b031b19a7fd3565a4cffc3c
Neillcllghn <neillcllghn@outlook.com>
2022-12-11 11:23:07 +0000

    Adding in the board types (Guessing and Hidden) with the column letters.

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+HIDDEN_BOARD = [[' ']*7 for x in range(6)]
+GUESS_PATTERN = [[' ']*7 for x in range(6)]
+let_to_nums={'A': 0,'B': 1, 'C': 2,'D': 3,'E': 4,'F': 5,'G': 6}
