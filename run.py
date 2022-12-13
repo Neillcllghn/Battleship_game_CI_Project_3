f0133d1012cd619c6034d61b0a43186505f36180
Neillcllghn <neillcllghn@outlook.com>
2022-12-13 18:50:29 +0000

    Correcting errors as per PEP8

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from random import randint
 
 HIDDEN_BOARD = [[' ']*7 for x in range(6)]
 GUESS_PATTERN = [[' ']*7 for x in range(6)]
-let_to_nums = {'A': 0,'B': 1, 'C': 2,'D': 3,'E': 4,'F': 5,'G': 6}
+let_to_nums = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6}
 
 
 def input_details():
@@ -16,10 +16,11 @@ def input_details():
         print('Must be no longer than 6 characters')
         print("Example: Neocal etc.....\n")
 
-        username = input("Enter your username here:\n")    
+        username = input("Enter your username here:\n")
         if validate_username(username):
             print(f'Hello {username}, Welcome to Battleship Madness!')
             break
+
 
 def validate_username(values):
     """
@@ -28,21 +29,25 @@ def validate_username(values):
     2. Contains no numbers or special characters.
     """
     if len(values) < 3 or len(values) > 6:
-        print(f"Username must be more than 3 and less than 6 characters long, you provided {len(values)}.")
+        print("Username must be more than 3 and less than 6 characters long, "
+              f"you provided {len(values)}.")
         return False
     elif any(char.isnumeric() for char in values):
-        print(f"the username {values} cannot be used, please don't use numbers.")
+        print(f"the username {values} cannot be used, "
+              "please don't use numbers.")
         return False
     elif not values.isalnum():
-        print(f"the username {values} cannot be used, please don't use non-alphabetic characters.")
+        print(f"the username {values} cannot be used, "
+              "please don't use special characters.")
         return False
 
     return True
 
+
 def print_board(board):
     """
     This creates the board.
-    Credit for code goes to Knowledge Mavens - 
+    Credit for code goes to Knowledge Mavens -
     How to Code Battleship in Python - Single Player Game.
     """
     print('  A B C D E F G')
@@ -52,15 +57,17 @@ def print_board(board):
         print("%d|%s|" % (row_of_numbers, "|".join(row)))
         row_of_numbers += 1
 
+
 def no_of_ships():
     """
     This is to allow the user to select the number of ships they want.
-    Inspired by Knowledge Mavens - 
+    Inspired by Knowledge Mavens -
     How to Code Battleship in Python - Single Player Game.
     """
     ships = input('How many ships do you want to sink? Between 1-10 :\n')
     while not ships.isdigit() or int(ships) < 1 or int(ships) >= 11:
-        print(f'You selected invaild {ships} number of ships, please try again')
+        print(f'You selected invaild {ships} number of ships, '
+              'please try again')
         ships = input('How many ships do you want to sink? Between 1-10 :\n')
     return int(ships)
 
@@ -68,25 +75,26 @@ def no_of_ships():
 def ship_creation(board):
     """
     This creates the ships and places them on the board randomly.
-    Inspired by Knowledge Mavens - 
+    Inspired by Knowledge Mavens -
     How to Code Battleship in Python - Single Player Game.
     """
     global ships
     ships = no_of_ships()
     for ship in range(ships):
-        ship_row, ship_column = randint(0,5), randint(0,5)
+        ship_row, ship_column = randint(0, 5), randint(0, 5)
         while board[ship_row][ship_column] == 'X':
-            ship_row, ship_column = randint(0,5), randint(0,5)
+            ship_row, ship_column = randint(0, 5), randint(0, 5)
         board[ship_row][ship_column] = 'X'
+
 
 def ship_location_choices():
     """
     Allows the user to input a row number and a column letter to guess where
     the ships are on the board.
-    Inspired by Knowledge Mavens - 
+    Inspired by Knowledge Mavens -
     How to Code Battleship in Python - Single Player Game.
     """
-    column_select = ['A','B','C','D','E','F','G']
+    column_select = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
     row = input("Please enter a ship row 1-6:\n")
     while not row.isdigit() or int(row) < 1 or int(row) > 6:
         print(f'You selected invaild {row} row, please try again')
@@ -97,10 +105,11 @@ def ship_location_choices():
         column = input('Please enter a ship column A-G:\n').upper()
     return int(row) - 1, let_to_nums[column]
 
+
 def count_hit_ships(board):
     """
     This reduces the number of ships if the user guessed correctly.
-    Credit for code goes to Knowledge Mavens - 
+    Credit for code goes to Knowledge Mavens -
     How to Code Battleship in Python - Single Player Game.
     """
     count = 0
@@ -110,21 +119,24 @@ def count_hit_ships(board):
                 count += 1
     return count
 
+
 def no_of_turns():
     """
     This is to allow the user to select the number of turns they want.
     """
     turns = input('How many turns do you want? Between 1-20 :\n')
     while not turns.isdigit() or int(turns) < 1 or int(turns) >= 21:
-        print(f'You selected invaild {turns} number of turns, please try again')
+        print(f'You selected invaild {turns} number of turns, '
+              'please try again')
         turns = input('How many turns do you want? Between 1-20 :\n')
     return int(turns)
 
+
 def game_logistics():
     """
-    The game function - what is needed to win, how many turns you have 
+    The game function - what is needed to win, how many turns you have
     (based on what was selected by the user), and when the game ends.
-    Inspired by Knowledge Mavens - 
+    Inspired by Knowledge Mavens -
     How to Code Battleship in Python - Single Player Game.
     """
     ship_creation(HIDDEN_BOARD)
@@ -155,8 +167,9 @@ def game_logistics():
             break
 
 
-def new_game ():
+def new_game():
     login_data = input_details()
     game = game_logistics()
 
-new_game ()
+
+new_game()
