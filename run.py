718fd826eeac6ccb0a3480b4dcd9fb680989ad23
Neillcllghn <neillcllghn@outlook.com>
2022-12-13 18:56:50 +0000

    adding credit notes

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from random import randint
 
+# credit to Knowledge Mavens - How to Code Battleship in Python
 HIDDEN_BOARD = [[' ']*7 for x in range(6)]
 GUESS_PATTERN = [[' ']*7 for x in range(6)]
 let_to_nums = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6}
